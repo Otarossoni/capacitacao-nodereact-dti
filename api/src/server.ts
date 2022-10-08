7c6d008ee195c505a6b9ae8520dd7d57e4f468cc
Otarossoni <otarossoni@gmail.com>
2022-10-08 10:02:38 -0300

    Estrutura de rotas da API

---
+++ api/src/server.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+import routes from "./routes/routes";
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
